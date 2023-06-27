1fc5dd996ca1da676b20a1591bb671e024fec241
Rex <s46129@yahoo.com.tw>
2023-06-28 01:35:20 +0800

    feat : Implement Uno Start

--- TemplatePractice/Uno/UnoGame.cs
+++ TemplatePractice/Uno/UnoGame.cs
@@ -1,10 +1,26 @@
-﻿namespace TemplatePractice.Uno;
+﻿using System;
+
+namespace TemplatePractice.Uno;
 
 internal class UnoGame : GameFlow
 {
+    private UnoCard tableCard;
+
     protected override Deck CreateDeck()
     {
         return new UnoDeck();
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        SetTableCard(Deck.DrawCard());
+    }
+
+    private void SetTableCard(Card card)
+    {
+        tableCard = card as UnoCard;
+        Console.WriteLine($"table card is {tableCard}");
     }
 
     protected override Player CreateHunamdPlayer()
@@ -16,5 +32,4 @@ internal class UnoGame : GameFlow
     {
         return new UnoComputerPlayer();
     }
-
 }
