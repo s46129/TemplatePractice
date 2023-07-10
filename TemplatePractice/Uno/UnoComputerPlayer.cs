0f0a4c4a0eb1f8b875ce0e230fabb7bb1cf5dcd4
Rex <s46129@yahoo.com.tw>
2023-07-10 23:06:36 +0800

    feat : Uno AI show card

--- TemplatePractice/Uno/UnoComputerPlayer.cs
+++ TemplatePractice/Uno/UnoComputerPlayer.cs
@@ -1,9 +1,15 @@
-﻿namespace TemplatePractice.Uno;
+﻿using System;
+
+namespace TemplatePractice.Uno;
 
 internal class UnoComputerPlayer : Player
 {
+    private static readonly Random Random = new Random();
+
     public override Card ShowCard()
     {
-        throw new System.NotImplementedException();
+        // AI 隨機選一張牌出
+        var parse = Random.Next(0, Hand._cards.Count);
+        return Hand._cards[parse];
     }
 }
